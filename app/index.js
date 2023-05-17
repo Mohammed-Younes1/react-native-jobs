470b8a979c31231d069e3b3ff4d91a947d9eb417
Mohammed Younes <mohammed.alyounes@gmail.com>
2023-05-17 10:33:47 +0300

    fixing git

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import "expo-router/entry";
+import {view,Text} from 'react-native';
+
+const Home=()=>{
+    return(
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+export default Home;
