d332d42436f1449dd1d243f394f65b1b8adfd79e
Mohammed Younes <mohammed.alyounes@gmail.com>
2023-05-21 15:26:38 +0300

    job details

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -20,7 +20,7 @@ import {
 import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hook/useFetch";
 
-const tabs=["About","Qualifications","Responsibilites"];
+const tabs = ["About", "Qualifications", "Responsibilities"];
 
 const JobDetails = () => {
   const params = useSearchParams();
@@ -31,9 +31,36 @@ const JobDetails = () => {
   });
 
   const [refreshing, setRefreshing] = useState(false);
-  const [activeTab,setActiveTab]=useState(tabs[0]);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
 
   const onRefresh = () => {};
+
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "Qualifications":
+        return (
+          <Specifics
+            title="Qualifications"
+            points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
+          />
+        );
+      case "About":
+        return (
+          <JobAbout
+            info={data[0].job_description ?? "No data provided"}
+          />
+        )
+      case "Responsibilities":
+        return (
+          <Specifics
+            title="Responsibilities"
+            points={data[0].job_highlights?.Responsibilities ?? ["N/A"]}
+          />
+        );
+      default:
+        break;
+    }
+  };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -77,13 +104,16 @@ const JobDetails = () => {
                 location={data[0].job_country}
               />
               <JobTabs
-               tabs={tabs}
-               activeTab={activeTab}
-               setActiveTab={setActiveTab}
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
               />
+              {displayTabContent()}
             </View>
           )}
         </ScrollView>
+
+        <JobFooter url={data[0]?.job_google_link ?? 'https://careers.google.com/jobs/results/'} />
       </>
     </SafeAreaView>
   );
